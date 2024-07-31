1fbb046f192cb7dc7617817173d744df0fc76976
sirRoach-nfp <nathanieljosepascual.psjnhs@gmail.com>
2024-07-31 11:47:10 +0800

    test

--- frontEndEcommerce/src/requestMethod.js
+++ frontEndEcommerce/src/requestMethod.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:5000/api"
+//const BASE_URL = "http://localhost:5000/api"
+const BASE_URL = "https://ecommercewebapp-26m8.onrender.com/api";
 let TOKEN = "";
 try {
     const rootData = JSON.parse(localStorage.getItem("persist:root"));
